01aa77bd49a0984aa5e66e15d72dc61e08f49e44
Çağrı Aldemir <cagri.aldemir@hotmail.com>
2020-04-21 01:56:58 +0300

    Angular eklemeleri yapıldı.

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'Angular';
+  selectBoxDatas: { id: number; value: string }[] = [
+    { id: 1, value: "Yağlar" },
+    { id: 2, value: "Deodorantlar" },
+    { id: 3, value: "HHC" },
+    { id: 4, value: "Ambalaj" },
+    { id: 5, value: "Meme" },
+  ];
+
+  selectedData: { id: number; value: string };
+
+  constructor(private http: HttpClient) {}
+
+  changed(selectedID: number) {
+    //    this.http.post
+  }
 }
