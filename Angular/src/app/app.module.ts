6f664e8a7a6068690e1317e01087208ba5ce99c4
Çağrı Aldemir <cagri.aldemir@hotmail.com>
2020-05-17 03:26:45 +0300

    PivotTable import edildi.

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { JqueryTestComponent } from './jquery-test/jquery-test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JqueryTestComponent
   ],
   imports: [
     BrowserModule,
---
+++ Angular/src/app/jquery-test/jquery-test.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit, ElementRef, Inject } from "@angular/core";
+import { Papa } from "ngx-papaparse";
+declare var jQuery: any;
+declare var $: any;
+
+@Component({
+  selector: "app-jquery-test",
+  templateUrl: "./jquery-test.component.html",
+  styleUrls: ["./jquery-test.component.scss"],
+})
+export class JqueryTestComponent implements OnInit {
+  private el: ElementRef;
+
+  constructor(@Inject(ElementRef) el: ElementRef, private papa: Papa) {
+    this.el = el;
+  }
+
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    if (!this.el || !this.el.nativeElement || !this.el.nativeElement.children) {
+      console.log("cant build without element");
+      return;
+    }
+
+    var container = this.el.nativeElement;
+    var inst = jQuery(container);
+    var targetElement = inst.find("#output");
+
+    if (!targetElement) {
+      console.log("cant find the pivot element");
+      return;
+    }
+
+    while (targetElement.firstChild) {
+      targetElement.removeChild(targetElement.firstChild);
+    }
+
+    let abc = this;
+    $(function () {
+      abc.papa.parse(
+        "https://raw.githubusercontent.com/eoeroglu/itubtp/master/Python/data.csv?token=AB5KSMCQV6NH6OUXAA5KCCC6YCCAU",
+        {
+          download: true,
+          skipEmptyLines: true,
+          complete: function (parsed) {
+            debugger;
+            targetElement.pivotUI(parsed.data, {
+              rows: ["Petal.Length"],
+              cols: ["Petal.Width", "Species"],
+            });
+          },
+        }
+      );
+    });
+  }
+}
